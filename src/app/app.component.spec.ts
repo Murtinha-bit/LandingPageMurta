34322eb08a7139e3055a14c001af6728f2aac320
Gabriel Murta De Freitas <gabrielmurta7@gmail.com>
2021-11-21 13:23:17 -0300

    commit inicial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LandingPage'`, () => {
+  it(`should have as title 'FAQPage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LandingPage');
+    expect(app.title).toEqual('FAQPage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LandingPage app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('FAQPage app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HomeFaqComponent } from './home-faq/home-faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    HomeFaqComponent
   ],
   imports: [
     BrowserModule
